c5c349ba937b128a58dead84ba5a92c681194b43
Jim Denton <denton.jim@gmail.com>
2020-02-29 22:05:24 -0700

    polybar config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,9 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log /tmp/polybar4.log
-polybar rwspace >>/tmp/polybar1.log 2>&1 &
-polybar rclock >>/tmp/polybar2.log 2>&1 &
-polybar rdate >>/tmp/polybar4.log 2>&1 &
+echo "---" | tee -a /tmp/polybar1.log
+polybar main >>/tmp/polybar1.log 2>&1 &
 
 echo "Polybar launched..."
